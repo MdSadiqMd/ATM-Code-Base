22009ef49b35458e8b41ef0238713f3b73362ae1
Md.Sadiq <mohammadsadiq4950@gmail.com>
2023-06-09 22:45:28 +0530

    Refactor --> Updated the Methods

--- Master Control/src/Option_Selector.java
+++ Master Control/src/Option_Selector.java
@@ -1,17 +1,19 @@
 import java.util.*;
 import java.io.IOException;
 import java.text.DecimalFormat;
-public class Option_Selector extends User_Account {
-    Scanner UserInput=new Scanner(System.in);
-    DecimalFormat CurrencyFormat=new DecimalFormat("'$'###,##0.00");
 
-    HashMap<Integer,Integer> data=new HashMap<Integer,Integer>();
-    public void getLogin() throws IOException{
-        int x=1;
-        do{
-            try{
-                data.put(952141,191904);
-                data.put(989947,71976);
+public class Option_Selector extends User_Account {
+    Scanner UserInput = new Scanner(System.in);
+    DecimalFormat CurrencyFormat = new DecimalFormat("'$'###,##0.00");
+
+    HashMap<Integer, Integer> data = new HashMap<Integer, Integer>();
+
+    public void getLogin() throws IOException {
+        int x = 1;
+        do {
+            try {
+                data.put(952141, 191904);
+                data.put(989947, 71976);
                 System.out.println("Welcome to the ATM");
 
                 System.out.println("Enter your Customer ID");
@@ -19,99 +21,96 @@ public class Option_Selector extends User_Account {
 
                 System.out.println("Enter your PIN Number");
                 setPinNumber(UserInput.nextInt());
-            } catch (Exception e){
-                System.out.println("""
-                        Enter the Valid Input
-                        """);
-                x=2;
+            } catch (Exception e) {
+                System.out.println("Enter the Valid Input");
+                x = 2;
             }
-            int ci=getCustomerNumber();
-            int pin=getPinNumber();
-            if(data.containsKey(ci)&&data.get(ci)==pin){
+            int ci = getCustomerNumber();
+            int pin = getPinNumber();
+            if (data.containsKey(ci) && data.get(ci) == pin) {
                 getAccountType();
-            } else{
-                System.out.println("""
-                        Enter valid Input
-                        """);
-            } while(x==1);
-        }
-        public void getAccountType(){
-            System.out.println("""
-                    Select the Service you want to Avail
-                    1. Checking Account
-                    2. Saving Account
-                    3. Exit
-                    """);
-            int selection =UserInput.nextInt();
-
-            switch (selection) {
-                case 1 -> getChecking();
-                case 2 -> getSaving();
-                case 3 -> System.out.println("Thank you for visiting");
-                default -> System.out.println("Please select the Service");
+            } else {
+                System.out.println("Enter valid Input");
             }
-        }
-        public void getChecking(){
-            System.out.println("Select the Service you want to Avail");
-            System.out.println("""
-                    1. View Balance
-                    2. With Draw Funds
-                    3. Deposit Funds
-                    4. Exit
-                    """);
-            int selection=UserInput.nextInt();
+        } while (x == 1);
+    }
 
-            switch(selection){
-                case 1:
-                    System.out.println("Checking Account Balance: "+moneyFormat.format(getChecking));
-                    getAccountType();
-                    break;
-                case 2:
-                    getCheckingWithDraw();
-                    getAccountType();
-                    break;
-                case 3:
-                    getCheckingDeposit();
-                    getAccountType();
-                    break;
-                case 4:
-                    System.out.println("Thank you for visiting");
-                default:
-                    System.out.println("""
-                            Invalid Choice
-                            """);
-            }
-        }
-        public void getSaving(){
-            System.out.println("Select the Service you want to Avail");
-            System.out.println("""
-                    1. View Balance
-                    2. With Draw Funds
-                    3. Deposit Funds
-                    4. Exit
-                    """);
-            int selection=UserInput.nextInt();
+    public void getAccountType() {
+        System.out.println("Select the Service you want to Avail");
+        System.out.println("""
+                1. Checking Account
+                2. Saving Account
+                3. Exit
+                """);
+        int selection = UserInput.nextInt();
 
-            switch(selection){
-                case 1:
-                    System.out.println("Checking Account Balance: "+moneyFormat.format(getSaving));
-                    getAccountType();
-                    break;
-                case 2:
-                    getSavingWithDraw();
-                    getAccountType();
-                    break;
-                case 3:
-                    getSavingDeposit();
-                    getAccountType();
-                    break;
-                case 4:
-                    System.out.println("Thank you for visiting");
-                default:
-                    System.out.println("""
-                            Invalid Choice
-                            """);
-            }
+        switch (selection) {
+            case 1 -> getChecking();
+            case 2 -> getSaving();
+            case 3 -> System.out.println("Thank you for visiting");
+            default -> System.out.println("Please select the Service");
+        }
+    }
+
+    public void getChecking() {
+        System.out.println("Select the Service you want to Avail");
+        System.out.println("""
+                1. View Balance
+                2. With Draw Funds
+                3. Deposit Funds
+                4. Exit
+                """);
+        int selection = UserInput.nextInt();
+
+        switch (selection) {
+            case 1:
+                System.out.println("Checking Account Balance: " + CurrencyFormat.format(getCheckingBalance()));
+                getAccountType();
+                break;
+            case 2:
+                getCheckingWithdraw();
+                getAccountType();
+                break;
+            case 3:
+                getCheckingDeposit();
+                getAccountType();
+                break;
+            case 4:
+                System.out.println("Thank you for visiting");
+                break;
+            default:
+                System.out.println("Invalid Choice");
+        }
+    }
+
+    public void getSaving() {
+        System.out.println("Select the Service you want to Avail");
+        System.out.println("""
+                1. View Balance
+                2. With Draw Funds
+                3. Deposit Funds
+                4. Exit
+                """);
+        int selection = UserInput.nextInt();
+
+        switch (selection) {
+            case 1:
+                System.out.println("Saving Account Balance: " + CurrencyFormat.format(getSavingBalance()));
+                getAccountType();
+                break;
+            case 2:
+                getSavingWithdraw();
+                getAccountType();
+                break;
+            case 3:
+                getSavingDeposit();
+                getAccountType();
+                break;
+            case 4:
+                System.out.println("Thank you for visiting");
+                break;
+            default:
+                System.out.println("Invalid Choice");
         }
     }
 }
